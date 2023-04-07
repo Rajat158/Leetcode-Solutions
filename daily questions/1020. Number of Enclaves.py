a1bc74e305e1b1cabb430a2ba2bebed97f876c9b
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-04-07 08:10:53 +0530

    Create 1020. Number of Enclaves.py

---
+++ daily questions/1020. Number of Enclaves.py
@@ -0,0 +1,43 @@
+'''
+You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+
+Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+Output: 3
+Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
+Example 2:
+
+
+Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
+Output: 0
+Explanation: All 1s are either on the boundary or can reach the boundary.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 500
+grid[i][j] is either 0 or 1.
+'''
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            grid[i][j] = 0
+            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                if 0 <= x < m and 0 <= y < n and grid[x][y]:
+                    dfs(x, y)
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (i == 0 or j == 0 or i == m - 1 or j == n - 1):
+                    dfs(i, j)
+        return sum(sum(row) for row in grid)
