2fef633375683c7df185e54054382999559ef286
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-06-21 19:01:01 +0530

    Update 2448. Minimum Cost to Make Array Equal.py

--- daily questions/2448. Minimum Cost to Make Array Equal.py
+++ daily questions/2448. Minimum Cost to Make Array Equal.py
@@ -33,18 +33,25 @@ n == nums.length == cost.length
 1 <= n <= 105
 1 <= nums[i], cost[i] <= 106
 '''
-+class Solution:
-+    def minCost(self, nums: List[int], cost: List[int]) -> int:
-+        size = len(nums)
-+        nums_costs = sorted([(nums[i], cost[i]) for i in range(size)], key=itemgetter(0))
-+        #nums_costs.item = (num, cost) sorted by num low to high
-+        
-+        left_index = 0
-+        left_cost_accrued = 0
-+        right_index = size - 1
-+        right_cost_accrued = 0
-+
-+        # TODO: Gather left and right costs
-+
-+        total_cost = left_cost_accrued + right_cost_accrued
-+        return total_cost
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        
+        def calculate_cost(l):
+            total_cost = 0
+            for i, x in enumerate(nums):
+                total_cost += abs(l - x) * cost[i]
+            return total_cost
+        
+  
+        left = min(nums)
+        right = max(nums) + 1
+        mid = (left + right) // 2
+        
+        while left < right:
+            if calculate_cost(mid) < calculate_cost(mid + 1):
+                right = mid
+            else:
+                left = mid + 1
+            mid = (left + right) // 2
+        
+        return calculate_cost(left)
