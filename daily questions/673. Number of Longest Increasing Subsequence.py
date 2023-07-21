5917f355955bbc6511b8b348db7cfacd5f32268f
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-07-21 15:17:02 +0530

    Create 673. Number of Longest Increasing Subsequence.py

---
+++ daily questions/673. Number of Longest Increasing Subsequence.py
@@ -0,0 +1,41 @@
+'''
+Given an integer array nums, return the number of longest increasing subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+Example 2:
+
+Input: nums = [2,2,2,2,2]
+Output: 5
+Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2000
+-106 <= nums[i] <= 106
+'''
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[1]*n
+        count=[1]*n
+        for i in range(1,n):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    if 1+dp[j]>dp[i]:
+                        dp[i]=dp[j]+1
+
+                        count[i]=count[j]
+
+                    elif dp[j]+1==dp[i]:
+                        count[i]+=count[j]
+        longest_len=max(dp)
+        return sum([count[i] for i in range(n) if dp[i]==longest_len])                
