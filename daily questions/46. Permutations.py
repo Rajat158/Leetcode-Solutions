3ec176e3d50a697315172b0298d69a9230c89738
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-08-02 20:34:00 +0530

    Create 46. Permutations.py

---
+++ daily questions/46. Permutations.py
@@ -0,0 +1,40 @@
+'''
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+'''
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def f(i,arr,res):
+            if(i>=len(arr)):
+                res.append(arr.copy())
+                return
+            for j in range(i,len(arr)):
+                arr[i],arr[j]=arr[j],arr[i]
+                f(i+1,arr,res)
+                arr[i],arr[j]=arr[j],arr[i]
+            return(res)
+        i=0
+        res=[]
+        f(i,nums,res)
+        return(res)
